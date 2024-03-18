07cae9e0bc45562276b2d2d8d2c64b62af61b91c
Computer <gogs@fake.local>
2024-03-18 14:55:12 +0500

    Внедрение зависимостей

---
+++ Equipments.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Equipments.Application.Interfaces;
+using Equipments.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Equipments.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionsString = configuration["DbDevConnection"];
+            services.AddDbContext<EquipmentsDbContext>(options =>
+            {
+                options.UseNpgsql(connectionsString);
+            });
+            services.AddScoped<IEquipmentsDbContext>(provider => provider.GetService<EquipmentsDbContext>());
+            return services;
+        }
+    }
+}
